9595989b41241ddfff37241dbc86a755c23d7714
Meraj Hossain Promit <merajhossainpromit@gmail.com>
2024-04-24 11:18:17 +0600

    deploy

---
+++ HMart/HMart/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from store_app.models import Product, Categories
+
+
+def BASE(request):
+    return render(request, 'Main/base.html')
+
+
+def HOME(request):
+    product = Product.objects.filter(status='PUBLISH')
+
+    context = {
+        'product': product,
+
+    }
+    return render(request, 'Main/index.html', context)
+
+
+def PRODUCT(request):
+    product = Product.objects.filter(status='PUBLISH')
+    categories = Categories.objects.all()
+
+    context = {
+        'product': product,
+        'categories': categories,
+    }
+    return render(request, 'Main/product.html')
